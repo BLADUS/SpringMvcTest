cb1b6729b3885d9f7e1ac87e7c888bb1cad1bd95
Vladislav Osada <osada.0339@gmail.com>
2023-04-28 14:02:37 +0300

    Model

--- src/main/java/ru/osada/spring/controllers/FirstController.java
+++ src/main/java/ru/osada/spring/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.osada.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,9 +17,10 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name",required = false) String name,
-                            @RequestParam(value = "surname",required = false) String surname) {
+                            @RequestParam(value = "surname",required = false) String surname,
+                            Model model) {
 
-        System.out.println("Имя: " + name + " Фамилия: " + surname);
+        model.addAttribute("userData","name: " + name + " surname: " + surname);
 
         return "first/hello";
     }
@@ -26,5 +28,35 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodBye() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a,
+                             @RequestParam("b") int b,
+                             @RequestParam("operator") String operator,
+                             Model model){
+        String result = null;
+        switch (operator){
+            case "addition":{
+                result = String.valueOf(a + b);
+                break;
+            }
+            case "multiplication":{
+                result = String.valueOf(a*b);
+                break;
+            }
+            case "subtraction":{
+                result = String.valueOf(a-b);
+                break;
+            }
+            case "division":{
+                result = String.valueOf(a/b);
+                break;
+            }
+            default:break;
+        }
+model.addAttribute("answer","Answer: " + result);
+
+        return "/first/calculator";
     }
 }
